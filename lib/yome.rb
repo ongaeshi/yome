c245eac89886d628d07a3d9cb4deb9edadfbbc44
ongaeshi <ongaeshi0621@gmail.com>
2018-05-26 20:02:37 +0900

    fix: Skip binary file

--- lib/yome.rb
+++ lib/yome.rb
@@ -5,11 +5,16 @@ module Yome
   def self.ignore?(path)
     path == ".git" || path == "oboe.rb"
   end
+
+  def self.binary?(path)
+    s = File.read(path, 1024) or return false
+    return s.index("\x00")
+  end 
 end
   
 Find.find(ARGV[0]) do |path|
   Find.prune if Yome::ignore?(File.basename(path))
-  next if FileTest.directory?(path)
+  next if FileTest.directory?(path) || Yome::binary?(path)
   
   open(path) do |io|
     io.each_line do |line|
